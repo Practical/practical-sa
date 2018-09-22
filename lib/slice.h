814e4e103a7a3eb2d79785f904df570173d73eee
Shachar Shemesh <shachar@shemesh.biz>
2018-09-22 12:35:45 +0300

    C++ UTs infra in place

--- lib/slice.h
+++ lib/slice.h
@@ -36,20 +36,28 @@ public:
         return subslice(start, len);
     }
 
-    const Slice subslice(size_t start) const {
+    Slice<const T> subslice(size_t start) const {
         return subslice(start, len);
     }
 
-    Slice<int> subslice(size_t start, size_t end) {
-        // It is legal to cast away constness if you know the underlying object is mutable
-        return ( static_cast<const Slice *>(this)->subslice(start, end) );
-        //return const_cast<Slice>( static_cast<const Slice *>(this)->subslice(start, end) );
-    }
+    Slice subslice(size_t start, size_t end) {
+        if( end<=start )
+            return Slice();
 
-    const Slice subslice(size_t start, size_t end) const {
         assert(start<len);
         assert(end<=len);
-        return Slice(ptr+start, len - start - end);
+
+        return Slice(ptr+start, end - start - 1);
+    }
+
+    Slice<const T> subslice(size_t start, size_t end) const {
+        if( end<=start )
+            return Slice<const T>();
+
+        assert(start<len);
+        assert(end<=len);
+
+        return Slice<const T>(ptr+start, end - start - 1);
     }
 };
 
--- lib/slice_ut.cpp
+++ lib/slice_ut.cpp
@@ -1,8 +1,42 @@
-#include <slice.h>
+#include <string.h>
+
 #include <cppunit/extensions/HelperMacros.h>
 
-CPPUNIT_TEST_SUITE_REGISTRATION( RangeTest );
+#include "slice.h"
 
-static void addMyTests() {
-    suite.addTest
-}
+class SliceTest : public CppUnit::TestFixture  {
+    void basicTest() {
+#define TEST_STR "Hello, world"
+        char string[] = TEST_STR;
+        auto slice = Slice<char>(string, sizeof(TEST_STR));
+        CPPUNIT_ASSERT( slice.length()==13 );
+        CPPUNIT_ASSERT( slice[0]=='H' );
+        CPPUNIT_ASSERT( slice[4]=='o' );
+        slice[1] = 'a';
+        CPPUNIT_ASSERT( strcmp(string, "Hallo, world")==0 );
+        CPPUNIT_ASSERT( slice[12] == '\0' );
+
+        auto subslice = static_cast<const Slice<char> &>(slice).subslice(1, 11);
+        CPPUNIT_ASSERT( subslice[1]=='l' );
+        CPPUNIT_ASSERT( subslice[subslice.length()-1]=='r' );
+        CPPUNIT_ASSERT( subslice.length()==9 );
+
+        auto mutableSubslice = slice.subslice(2, 12);
+        CPPUNIT_ASSERT( mutableSubslice[1]=='l' );
+        CPPUNIT_ASSERT( mutableSubslice[subslice.length()-1]=='l' );
+        CPPUNIT_ASSERT( mutableSubslice.length()==9 );
+
+    }
+
+public:
+    static CppUnit::Test *suite()
+    {
+        CppUnit::TestSuite *suiteOfTests = new CppUnit::TestSuite( "SliceTest" );
+        suiteOfTests->addTest( new CppUnit::TestCaller<SliceTest>(
+                    "basicTest",
+                    &SliceTest::basicTest ) );
+        return suiteOfTests;
+    }
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( SliceTest );
