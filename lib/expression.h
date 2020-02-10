3bbdf4f992a218be883b3fc10f95b4bd7dbe86b3
Shachar Shemesh <shachar@shemesh.biz>
2020-02-10 05:31:53 +0200

    Split ValueRange to its own header

--- lib/expression.h
+++ lib/expression.h
@@ -9,33 +9,13 @@
 #ifndef EXPRESSION_H
 #define EXPRESSION_H
 
-#include "full_range_int.h"
 #include "practical-sa.h"
+#include "value_range.h"
 
 #include <boost/intrusive_ptr.hpp>
 
 #include <iostream>
 #include <memory>
-
-struct ValueRange : private NoCopy, public boost::intrusive_ref_counter<ValueRange, boost::thread_unsafe_counter> {
-    FullRangeInt minimum, maximum;
-
-    ValueRange( FullRangeInt min, FullRangeInt max ) : minimum(min), maximum(max) {}
-
-    static boost::intrusive_ptr<const ValueRange> allocate( FullRangeInt min, FullRangeInt max ) {
-        return new ValueRange(min, max);
-    }
-
-    bool containedIn( const ValueRange &that ) const {
-        return minimum>=that.minimum && maximum<=that.maximum;
-    }
-};
-
-inline std::ostream &operator<<( std::ostream &out, const ValueRange &range ) {
-    out<<range.minimum<<" to "<<range.maximum;
-
-    return out;
-}
 
 struct Expression : private NoCopy {
     PracticalSemanticAnalyzer::ExpressionId id;
